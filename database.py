e88066a720c7698cd1c8f2c5de48eb9aacdb57b6
Munyaradzi Togarepi <directorpseudo@gmail.com>
2025-04-15 11:42:57 +0100

    Update database.py

--- database.py
+++ database.py
@@ -931,3 +931,49 @@ class Database:
                 return {"status": "success"}
         except Exception as e:
             return {"status": "Error: " + str(e)}
+
+
+    #open ad views for an advertiser
+    def open_ad_views(self, ad_id):
+        try:
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM ad_views WHERE ad_id=? ", (ad_id,))
+                views_pre = cursor.fetchall()
+                views=[]
+                for ad in views_pre:
+                    views.append({"user_id":ad[1],"chat_id":ad[2],"viewed_at":ad[3]})
+
+                return views
+        except Exception as e:
+            print("error: "+str(e))
+            return []
+
+    #open ad leads for an advertiser
+    def open_ad_leads(self, ad_id):
+        try:
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM ad_leads WHERE ad_id=? ", (ad_id,))
+                leads_pre = cursor.fetchall()
+                leads=[]
+                for ad in leads_pre:
+                    #ad_id, user_id, name, email, phone, status, viewed_at
+                    leads.append({"user_id":ad[1],"name":ad[2],"email":ad[3],"phone":ad[4],"status":ad[5]"created_at":ad[6]})
+
+                return leads
+        except Exception as e:
+            print("error: "+str(e))
+            return []
+
+
+    #close a lead
+    def close_lead(self, ad_id, user_id):
+        try:
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE ad_leads SET status='closed' WHERE ad_id=? AND user_id=?", (ad_id, user_id))
+                conn.commit()
+                return {'status':'success'}
+        except Exception as e:
+            return {"status": "Error: " + str(e)}
