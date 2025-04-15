d4494c0aa162cc7cbf5a13940af259aab0458393
Munyaradzi Togarepi <directorpseudo@gmail.com>
2025-04-15 11:20:20 +0100

    Update database.py

--- database.py
+++ database.py
@@ -23,6 +23,10 @@ class Database:
                               (user_id TEXT, file_id TEXT, filename TEXT, table_id TEXT, table_ TEXT, citation TEXT)''')
         cursor.execute('''CREATE TABLE IF NOT EXISTS superusers
                               (admin_id TEXT, name TEXT, email TEXT, password TEXT, created_at TEXT)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS ad_views
+                              (ad_id TEXT, user_id TEXT, chat_id TEXT, viewed_at TEXT)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS ad_leads
+                              (ad_id TEXT, user_id TEXT, name TEXT, email TEXT, phone TEXT, status TEXT, created_at TEXT)''')
 
         conn.commit()
         
@@ -916,12 +920,13 @@ class Database:
     def add_ad_lead(self, ad_id, user_id, name, email, phone):
         current_datetime = datetime.now()
         viewed_at = str(current_datetime.date())
+        status="open"
         try:
             with sqlite3.connect(self.db_path) as conn:
                 cursor = conn.cursor()
                 
-                cursor.execute("INSERT INTO ad_leads (ad_id, user_id, name, email, phone, viewed_at) VALUES (?,?,?,?,?,?)",
-                                   (ad_id, user_id, name, email, phone, viewed_at))
+                cursor.execute("INSERT INTO ad_leads (ad_id, user_id, name, email, phone, status, created_at) VALUES (?,?,?,?,?,?,?)",
+                                   (ad_id, user_id, name, email, phone, status, viewed_at))
                 conn.commit()
                 return {"status": "success"}
         except Exception as e:
